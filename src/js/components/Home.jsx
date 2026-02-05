68d261a78679e45f880b268af9bce4f1a70fed90
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-02-05 01:31:44 +0000

    added jumbotron section

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,28 +4,16 @@ import NewHTML from "./New";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { NavBar } from "./NavBar";
+import { Jumbo } from "./Jumbo";
 
 //create your first component
 const Home = () => {
-
-	
 
 	return (
 		<div className="text-center">
             
 			<NavBar/>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Jumbo/>
 		</div>
 	);
 };
