c28f558ee18a90b0e931775b33bb2087aa8a67f6
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-02-07 01:22:32 +0000

    update to jumbotron jsx file

--- src/js/components/Jumbo.jsx
+++ src/js/components/Jumbo.jsx
@@ -2,21 +2,18 @@ import React from "react";
 
 export const Jumbo = () => {
   return (
-    <div className="text-center">
+    <div className="w-75 mx-auto">
       <div className="p-5 mb-4 bg-body-tertiary rounded-3">
-        {" "}
-        <div className="container-fluid py-5">
-          {" "}
-          <h1 className="display-5 fw-bold">Custom jumbotron</h1>{" "}
+        <div className="container-fluid py-5 text-start">
+          <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
           <p className="col-md-8 fs-4">
-            Using a series of utilities, you can create this jumbotron, just
-            like the one in previous versions of Bootstrap. Check out the
-            examples below for how you can remix and restyle it to your liking.
-          </p>{" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
           <button className="btn btn-primary btn-lg" type="button">
-            Example button
-          </button>{" "}
-        </div>{" "}
+            Call to action!
+          </button>
+        </div>
       </div>
     </div>
   );
